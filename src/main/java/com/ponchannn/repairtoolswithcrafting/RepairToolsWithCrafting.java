45999c1a27d97b7afa01299bba0eb6afe19387ce
Luna T <luna_trn@tutanota.com>
2024-08-07 14:27:08 +1000

    refactor: Removed unecessary dependencies

--- src/main/java/com/ponchannn/repairtoolswithcrafting/RepairToolsWithCrafting.java
+++ src/main/java/com/ponchannn/repairtoolswithcrafting/RepairToolsWithCrafting.java
@@ -1,24 +1,18 @@
 package com.ponchannn.repairtoolswithcrafting;
-
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public final class RepairToolsWithCrafting extends JavaPlugin {
-    private static Listeners eventHandlers = null;
+    private Listeners eventHandlers = null;
 
     @Override
     public void onEnable() {
         eventHandlers = new Listeners();
         eventHandlers.registerListener(this);
-
     }
 
     @Override
     public void onDisable() {
+        this.eventHandlers = null;
         // Plugin shutdown logic
     }
 }
